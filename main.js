0625f0dbcab0b4c1df73981f897ea0646bd410d7
Stuart Wong <swong31@gmail.com>
2020-07-24 17:47:04 -0700

    created function cycle through questions and added bootstrap cdn

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var dataStructure = [
     {
-      q: "What Year was I born",
+      q: "What Year was I born?",
       a: ["1989", "1986", "1998", "1980"], 
       correctAnswer: "1980"
     },
@@ -11,18 +11,38 @@ var dataStructure = [
     }
   ]
 
-var questionDiv = document.querySelector("#question");
-var newQuestion = document.createElement("h1");
-newQuestion.textContent = dataStructure[0].q;
-questionDiv.appendChild(newQuestion);
+        var questionDiv = document.querySelector("#question");
+        var question = document.createElement("h1");
 
-var answersDiv = document.querySelector("#answers");
-var newAnswers = document.createElement("button");
-newAnswers.textContent = dataStructure[0].a;
-answersDiv.appendChild(newAnswers);
+        var a0Div = document.querySelector("#a0");
+        var newA0 = document.createElement("button");
 
-var test = document.createElement("h3")
-test.textContent = dataStructure[0].a[0]
-window.document.body.appendChild(test)
+        var a1Div = document.querySelector("#a1");
+        var newA1 = document.createElement("button");
 
+        var a2Div = document.querySelector("#a2");
+        var newA2 = document.createElement("button");
 
+        var a3Div = document.querySelector("#a3");
+        var newA3 = document.createElement("button");
+
+    function newQuestion () {
+      for (var i = 0; i < dataStructure.length; i++) {
+        
+        question.textContent = dataStructure[i].q;
+        questionDiv.appendChild(question);
+   
+        newA0.textContent = dataStructure[i].a[0];
+        a0Div.appendChild(newA0);
+       
+        newA1.textContent = dataStructure[i].a[1];
+        a1Div.appendChild(newA1);
+       
+        newA2.textContent = dataStructure[i].a[2];
+        a2Div.appendChild(newA2);
+      
+        newA3.textContent = dataStructure[i].a[3];
+        a3Div.appendChild(newA3);
+        }
+    }
+newQuestion()
