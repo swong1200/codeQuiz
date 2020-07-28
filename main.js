37f3c20d3e9673b735a8c9f542a55e4e3a829f02
Stuart Wong <swong31@gmail.com>
2020-07-28 10:42:08 -0700

    Initials are now visible in form and questions cycle through and populate correctly

--- main.js
+++ main.js
@@ -12,14 +12,14 @@ var form = document.querySelector("#form")
 // Using an object to store the questions and answers
 var dataStructure = [
     {
-      q: "What Year was I born?",
-      a: ["1989", "1986", "1998", "1980"], 
-      correctAnswer: "1980"
+      q: "Inside which HTML element do we put the Javascript?",
+      a: ["<javascripts>", "<scripting>", "<js>", "<script>"], 
+      correctAnswer: "<script>",
     },
     {
       q: "What do you call the type of value that determines if something is true or false?",
-      a: ["Boolean", "Variable","String", "Argument"], 
-      correctAnswer: "Boolean"
+      a: ["Boolean", "Variable", "String", "Argument"],
+      correctAnswer: "Boolean",
     },
     {
       q: "What punctuation is used to surround a string?",
@@ -28,8 +28,8 @@ var dataStructure = [
     }
   ];
 
-// Starting point for the questions
-var questionIndex = dataStructure[0];
+// Data structure index starting point
+var index = 0
 
 // Final question
 var questionFinal = dataStructure.length;
@@ -70,8 +70,7 @@ function countDown () {
   var timerInterval = setInterval(function() {
       timer.textContent = "Time Left: " + secondsLeft;
       secondsLeft--;
-      newQuestion();
-      if(secondsLeft === 0 || dataStructure.length === questionIndex) {
+      if(secondsLeft === 0 || questionFinal > questionIndex) {
           clearInterval(timerInterval);
           gameOver();
       }
@@ -86,6 +85,9 @@ a3Div.addEventListener("click", checkAnswer);
 
 //    Function to make questions appear
 function newQuestion () {
+  // Starting point for the questions
+  var questionIndex = dataStructure[index];
+  index++
   questionDiv.textContent = questionIndex.q;
   a0Div.textContent = questionIndex.a[0];
   a1Div.textContent = questionIndex.a[1];
@@ -99,12 +101,10 @@ function checkAnswer (e) {
         if (selectedButton === questionIndex.correctAnswer) {
           alert("Correct!");
           score++;
-          questionIndex[+1];
           newQuestion();
         } else {
           alert("Wrong!");
           secondsLeft = secondsLeft - 5;
-          questionIndex[+1];
           newQuestion();
         }
     }
@@ -116,5 +116,5 @@ function gameOver() {
     a1Div.classList.add("hide");
     a2Div.classList.add("hide");
     a3Div.classList.add("hide");
-    form.classList.remove("hide")
+    form.classList.remove("hide");
 }
